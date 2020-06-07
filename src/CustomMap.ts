427eea70ceddb18d28c658a5328d72149396c98a
Husein <nvdhunter007@gmail.com>
2020-06-07 22:54:32 +0700

    generalize add marker for Mappable class only

--- src/CustomMap.ts
+++ src/CustomMap.ts
@@ -1,5 +1,4 @@
-import { User } from "./User";
-import { Company } from "./Company";
+import { Mappable } from "./Mappable";
 
 export class CustomMap {
   private googleMap: google.maps.Map;
@@ -14,22 +13,12 @@ export class CustomMap {
     });
   }
 
-  addUserMarker(user: User): void {
+  addMarker(mappable: Mappable): void {
     new google.maps.Marker({
       map: this.googleMap,
       position: {
-        lat: user.location.lat,
-        lng: user.location.lng,
-      },
-    });
-  }
-
-  addCompanyMarker(company: Company): void {
-    new google.maps.Marker({
-      map: this.googleMap,
-      position: {
-        lat: company.location.lat,
-        lng: company.location.lng,
+        lat: mappable.location.lat,
+        lng: mappable.location.lng,
       },
     });
   }
--- src/index.ts
+++ src/index.ts
@@ -10,5 +10,5 @@ console.log(user, company);
 const customMap = new CustomMap("map");
 console.log(customMap);
 
-customMap.addUserMarker(user);
-customMap.addCompanyMarker(company);
+customMap.addMarker(user);
+customMap.addMarker(company);
